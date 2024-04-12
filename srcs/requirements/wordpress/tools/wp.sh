4c6c941a5f2cc14563420f069c7774a4c29722cb
Kichan Lee <kichlee@cx1r5s7.42seoul.kr>
2024-04-12 19:00:26 +0900

    sf

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -7,7 +7,8 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
     sleep 1
     wp core download --allow-root
     echo "core after"
-    wp config create --dbname=$MYSQL_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --path=/var/www/html --allow-root
+    #wp config create --dbname=$MYSQL_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --path=/var/www/html --allow-root
+    wp config create --dbname=wordpress --dbuser=kichlee --dbpass=1234 --dbhost=wordpress --path=/var/www/html --allow-root
     echo "config create "
     wp core install --url=localhost --title="Inception" --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --path=/var/www/html --allow-root
     wp user create $WORDPRESS_USER_NAME $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role="author" --path=/var/www/html --allow-root
